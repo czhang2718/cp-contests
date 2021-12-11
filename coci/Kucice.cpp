dcd87631cab872aabdded08454e026bbc8c0d99d
Claire Zhang <czhang2718@gmail.com>
2021-12-11 15:20:13 -0500

    edit coci E

--- coci/Kucice.cpp
+++ coci/Kucice.cpp
@@ -15,31 +15,8 @@ const double PI=3.14159265358979;
 int pw[N];
 double alpha[N];
 
-int add(int a, int b){
-  a+=b;
-  if(a<0) a+=MOD;
-  if(a>=MOD) a-=MOD;
-  return a;
-}
-
-int mult(int a, int b){
-  return (ll(a)*b)%MOD;
-}
-
-double aacos(double d){
-  d=max(-1.0, d);
-  d=min(1.0, d);
-  return acos(d);
-}
-
-double angle(int i, int j, int k){
-  pii u={x[i]-x[j], y[i]-y[j]};
-  pii v={x[k]-x[j], y[k]-y[j]};
-  double a=aacos((u.f*v.f+u.s*v.s) / sqrt(u.f*u.f+u.s*u.s) / sqrt(v.f*v.f+v.s*v.s));
-
-  if(a<0) a+=2*PI;
-  if(u.f*v.s-u.s*v.f>0) a=2*PI-a;
-  return a;
+double angle(int i, int j){
+  return atan2(x[i]-x[j], y[i]-y[j]);
 }
 
 int main(){
@@ -59,14 +36,13 @@ int main(){
     cin >> x[i] >> y[i];
   }
 
-  int ans=0;
+  ll ans=0;
 
   for(int i=0; i<n; i++){
     vector<double> pts;
-    int in=n;
     for(int j=0; j<n; j++){
       if(j==i) continue;
-      pts.push_back(angle(in, i, j));
+      pts.push_back(angle(i, j));
     }
     sort(pts.begin(), pts.end());
 
@@ -78,12 +54,12 @@ int main(){
         while(q.size() && pts[j]-q.front()>=PI){
           q.pop();
         }
-        if(!it) ad+=pw[n-2-q.size()];
+        if(!it) ad=(ad+pw[n-2-q.size()])%MOD;
         q.push(pts[j]);
         pts[j]+=2*PI;
       }
     }
-    ans=add(ans, pw[n]-ad-1);
+    ans=(ans+pw[n]-ad-1+MOD)%MOD;
   }
 
   cout << ans;
